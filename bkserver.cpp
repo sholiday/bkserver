1e2981f9bd2d48b15abc4e666a10ef574b4f09b0
Stephen Holiday <stephen.holiday@gmail.com>
2011-07-10 18:20:48 -0400

    Proper HTTP query params, JSON response.
    -- From Central Park

--- bkserver.cpp
+++ bkserver.cpp
@@ -32,47 +32,65 @@
 #include <stdio.h>
 
 BKTree<std::string> tree;
-std::string t("hefy");
+
+static void get_qsvar(const struct mg_request_info *request_info, const char *name, char *dst, size_t dst_len) {
+    const char *qs = request_info->query_string;
+    mg_get_var(qs, strlen(qs == NULL ? "" : qs), name, dst, dst_len);
+}
+
 void *callback(enum mg_event event,
                       struct mg_connection *conn,
                       const struct mg_request_info *request_info) {
   if (event == MG_NEW_REQUEST) {
     // Echo requested URI back to the client
-    mg_printf(conn, "HTTP/1.1 200 OK\r\n\r\n");
+    mg_printf(conn, "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n");
     
-    char * pch;
-    pch = strtok (request_info->query_string,"-");
+    std::string edit_distance_str;
+    std::string search_word;
     
-    std::string edit_distance_str(pch);
-    std::cout<<edit_distance_str;
-    pch = strtok (NULL,"-");
-    std::string search_word(pch);
+    char cb[255];
+
+    get_qsvar(request_info, "query", cb, sizeof(cb));
+    if (cb[0] != '\0') {
+        search_word=std::string(cb);
+    } else {
+        search_word=std::string("");
+    }
+    
+    get_qsvar(request_info, "max_distance", cb, sizeof(cb));
+    if (cb[0] != '\0') {
+        edit_distance_str=std::string(cb);
+    } else {
+        edit_distance_str=std::string("1");
+    }
     
     int edit_distance=atoi(edit_distance_str.c_str());
     
-    mg_printf(conn, "Query: %s\r\n",search_word.c_str());
+    mg_printf(conn, "{\"success\":true, \"query\":\"%s\",\"max_distance\": %d, ",search_word.c_str(),edit_distance);
     
     std::vector<std::string> results;
     results=tree.find(search_word,edit_distance);
     
 
     std::vector<std::string>::iterator it;
-    mg_printf(conn, "Results: %d\r\n",results.size());
-    for ( it=results.begin() ; it < results.end(); it++ )
-        mg_printf(conn,"%s\r\n",(*it).c_str());
+    mg_printf(conn, "\"result_size\": %d, \"results\":[",results.size());
+    bool first=true;
+    for ( it=results.begin() ; it < results.end(); it++ ) {
+        if (first) {
+            mg_printf(conn,"\"%s\"",(*it).c_str());
+            first=false;
+        } else {
+            mg_printf(conn,",\"%s\"",(*it).c_str());
+        }
+    }
         //std::cout << *it << std::endl;
-    
+    mg_printf(conn,"]}");
     
     return 0;  // Mark as processed
   } else {
     return NULL;
   }
 }
-
-int parse_distance(const char *query) {
-    //return atoi(query[1]);
-}
-
 
 int main(int argc, char* argv[]) { 
     std::cout<<"Loading BKTree..."<<std::endl;
@@ -88,9 +106,11 @@ int main(int argc, char* argv[]) {
     struct mg_context *ctx;
     const char *options[] = {"listening_ports", "8080", NULL};
 
-    std::cout << "Starting Server....";
+    std::cout << "Starting Server...."<<std::endl;
     ctx = mg_start(&callback, NULL, options);
     getchar();  // Wait until user hits "enter"
+    getchar();  // Wait until user hits "enter again"
+    std::cout << "Stopping Server...."<<std::endl;
     mg_stop(ctx);
 
   return 0;
